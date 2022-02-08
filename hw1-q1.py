b45757f79e33da62ecf6c7f998b82a8a490a27e1
NSkyeKirsch <73858119+NSkyeKirsch@users.noreply.github.com>
2022-02-07 20:12:43 -0500

    Hw1-q1 complete

---
+++ hw1-q1.py
@@ -0,0 +1,13 @@
+"""
+Author: Noa Kirschbaum
+Assignment / Part: HW1 - Q1
+Date due: 2020-02-10
+I pledge that I have completed this assignment without
+collaborating with anyone else, in conformance with the
+NYU School of Engineering Policies and Procedures on
+Academic Misconduct.
+"""
+
+name_input = input("Please enter your name: ");
+print("Hi",name_input,"Welcome to CS UY 1114! In this class you will learn Python and problem solving!");
+
