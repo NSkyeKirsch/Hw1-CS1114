1ed3fae5192cf70907f4363b1bcdf9a27bb75983
NSkyeKirsch <73858119+NSkyeKirsch@users.noreply.github.com>
2022-02-07 20:53:42 -0500

    Hw1-q2 complete

--- hw1-q2.py
+++ hw1-q2.py
@@ -7,14 +7,16 @@ collaborating with anyone else, in conformance with the
 NYU School of Engineering Policies and Procedures on
 Academic Misconduct.
 """
+seconds_in_year = 31536000;
+original_population = 330109174;
 
 year_input = int(input("How many years in the future? "));
 
-num_births = (year_input*31536000)//7; #the big number is seconds in a year
-num_deaths = (year_input*31536000)//13;
-num_immigrants = (year_input*31536000)//35;
+num_births = (year_input*seconds_in_year)//7;
+num_deaths = (year_input*seconds_in_year)//13;
+num_immigrants = (year_input*seconds_in_year)//35;
 
-est_population = num_births + num_immigrants - num_deaths;
+est_population = (num_births + num_immigrants - num_deaths) + original_population;
 
 print("the estimated population in",year_input,"years is",est_population);
 
